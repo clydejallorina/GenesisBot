2cadff6bed06979fac2eda91785dc73779647b86
clydejallorina <clydejallorina@gmail.com>
2016-08-13 20:59:00 +0800

    iRevert "Revert "fixed local merge""
    This reverts commit 969f14a1154b1d333b633e2cf7fd3749a1c875d7.
    
    ahh

---
+++ GenesisBot.js
@@ -0,0 +1,98 @@
+var fs = require("fs");
+var Discord = require("discord.js");
+var token = String(retrieveText("token.txt"));
+var botClient = new Discord.Client({autoReconnect: true});
+
+var token = fs.readFileSync("token.txt", {encoding: 'utf8'}).trim();
+
+var spectatorID = "207396959789514752";
+var newcomerID = "207396529386946560";
+var crewID = "209692193785380864";
+
+botClient.on("message" , function(message) {
+    if (message.content.startsWith("!"))
+    {
+        if (message.content == "!join")
+        {
+            if (botClient.memberHasRole(message.author , newcomerID))
+            {
+                botClient.addMemberToRole(message.author , spectatorID , function(err){
+                    if (err)
+                    {
+                        console.log("Unable to turn member " + message.author.name + " to a Spectator!");
+                        console.log("Error: " , err);
+                    }
+                    else
+                    {
+                        console.log("Member turned! Now to remove the newcomer role...");
+                        botClient.removeMemberFromRole(message.author , newcomerID , function(err){
+                        if (err != null || err != undefined)
+                        {
+                            console.log("Unable to remove newcomer role");
+                            console.log("Error: " , err);
+                        }    
+                        else
+                        {
+                            console.log("And it worked!");
+                        }
+                        });
+                    }
+                });
+            }
+        }
+    }
+    
+    else if (message.content == "=logout")
+    {
+        botClient.destroy(function(err){
+            console.log("Are you fucking kidding me");
+        })
+    }
+    
+    else if (message.content.startsWith("=getroleid"))
+    {
+        var something = message.content.split(" ");
+        something = something.splice(-1,1);
+        var x = "";
+        for (var i = 0; i < something.length; i++)
+        {
+            x = x + something[i];
+        }
+        console.log(x);
+        var role = message.channel.server.roles.get("name" , x);
+        if (role != null)
+            botClient.reply(message, role.id);
+        else
+            botClient.reply(message, "Role not found! (Role lookup is case sensitive!)");
+    }
+});
+
+botClient.on("serverNewMember" , function(server , user){
+    if (server.id == "207045717687009280")
+    {
+        botClient.addMemberToRole(user , "207396529386946560" , function(err){
+            if (err)
+            {
+                console.log("Unable to turn member " + user.name + " to a newcomer!");
+                console.log("Error: " , err);
+            }
+            else
+            {
+                console.log("NEW MEMBER!");
+            }
+        })
+    }
+})
+
+console.log("Logging in with token", token);
+botClient.loginWithToken(token , function (err) {
+    if (err)
+    {
+        console.log("Bot login failed!");
+        console.log("Error: " , err);
+    }
+    else
+    {
+        console.log("Bot login successful!");
+    }
+})
