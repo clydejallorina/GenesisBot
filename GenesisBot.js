285ce03db4a7e5491f9200ef4246bd9cbe894986
clydejallorina <clydejallorina@gmail.com>
2016-08-13 21:51:02 +0800

    Fixed GenesisBot.js locally

--- GenesisBot.js
+++ GenesisBot.js
@@ -1,98 +1,71 @@
-var fs = require("fs");
-var Discord = require("discord.js");
-var token = String(retrieveText("token.txt"));
-var botClient = new Discord.Client({autoReconnect: true});
+const fs = require("fs");
+const _ = require('lodash');
+const winston = require('winston');
+const Discord = require("discord.js");
+const client = new Discord.Client({autoReconnect: true});
 
-var token = fs.readFileSync("token.txt", {encoding: 'utf8'}).trim();
+const config = _.merge(require('./config.json'), require('./secrets.json'));
 
+// role ids
+// TODO: replace these with name lookups at some point
 var spectatorID = "207396959789514752";
 var newcomerID = "207396529386946560";
 var crewID = "209692193785380864";
 
-botClient.on("message" , function(message) {
-    if (message.content.startsWith("!"))
-    {
-        if (message.content == "!join")
-        {
-            if (botClient.memberHasRole(message.author , newcomerID))
-            {
-                botClient.addMemberToRole(message.author , spectatorID , function(err){
-                    if (err)
-                    {
-                        console.log("Unable to turn member " + message.author.name + " to a Spectator!");
-                        console.log("Error: " , err);
-                    }
-                    else
-                    {
-                        console.log("Member turned! Now to remove the newcomer role...");
-                        botClient.removeMemberFromRole(message.author , newcomerID , function(err){
-                        if (err != null || err != undefined)
-                        {
-                            console.log("Unable to remove newcomer role");
-                            console.log("Error: " , err);
-                        }    
-                        else
-                        {
-                            console.log("And it worked!");
-                        }
+client.on("message" , function(message) {
+    if (message.content.startsWith("!")) {
+        if (message.content == "!join") {
+            if (client.memberHasRole(message.author, newcomerID)) {
+                client.addMemberToRole(message.author, spectatorID, function(err) {
+                    if (err) {
+                        winston.error("Error giving user", message.author.name, "the Spectator role:", err);
+                    } else {
+                        winston.info("Spectator role added to user " + message.author.name + ". Removing newcomer role and deleting !join message...");
+                        client.deleteMessage(message);
+                        client.removeMemberFromRole(message.author , newcomerID , function(err){
+                            if (err) {
+                                winston.error("Error removing newcomer role:", err);
+                            } else {
+                                winston.info("Success.");
+                            }
                         });
                     }
                 });
             }
         }
-    }
-    
-    else if (message.content == "=logout")
-    {
-        botClient.destroy(function(err){
-            console.log("Are you fucking kidding me");
-        })
-    }
-    
-    else if (message.content.startsWith("=getroleid"))
-    {
-        var something = message.content.split(" ");
-        something = something.splice(-1,1);
-        var x = "";
-        for (var i = 0; i < something.length; i++)
-        {
-            x = x + something[i];
-        }
-        console.log(x);
-        var role = message.channel.server.roles.get("name" , x);
+    } else if (message.content == "=logout") {
+        client.destroy(function(err){
+            winston.info("Are you fucking kidding me?! *dies a gristly death*");
+        });
+    } else if (message.content.startsWith("=getroleid")) {
+        // drop the =getroleid at the beginning of the string
+        var roleName = message.content.split(' ').splice(1).join(' ');
+
+        var role = message.channel.server.roles.get("name" , roleName);
         if (role != null)
-            botClient.reply(message, role.id);
+            client.reply(message, "Role " + roleName + " has id " + role.id);
         else
-            botClient.reply(message, "Role not found! (Role lookup is case sensitive!)");
+            client.reply(message, "Role not found! (Role lookup is case sensitive!)");
     }
 });
 
-botClient.on("serverNewMember" , function(server , user){
-    if (server.id == "207045717687009280")
-    {
-        botClient.addMemberToRole(user , "207396529386946560" , function(err){
-            if (err)
-            {
-                console.log("Unable to turn member " + user.name + " to a newcomer!");
-                console.log("Error: " , err);
+client.on("serverNewMember", function(server, user) {
+    if (server.id == config.serverID) {
+        client.addMemberToRole(user, newcomerID, function(err) {
+            if (err) {
+                winston.error("Error giving user", user.name, "the newcomer role:", err);
+            } else {
+                winston.info("A new member,", user.name, "joined and was given the newcomer role.");
             }
-            else
-            {
-                console.log("NEW MEMBER!");
-            }
-        })
+        });
     }
-})
+});
 
-console.log("Logging in with token", token);
-botClient.loginWithToken(token , function (err) {
-    if (err)
-    {
-        console.log("Bot login failed!");
-        console.log("Error: " , err);
+winston.info("Logging in with token", config.token);
+client.loginWithToken(config.token, function (err) {
+    if (err) {
+        winston.info("Error logging in to Discord:", err);
+    } else {
+        winston.info("Bot login successful!");
     }
-    else
-    {
-        console.log("Bot login successful!");
-    }
-})
+});
